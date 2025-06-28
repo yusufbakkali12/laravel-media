b9db77a86f40b3ced6ffd32c21e2037ba5176e20
Bakkali youssef <bakkali1998yf@gmail.com>
2025-06-28 01:09:43 +0100

    Refactor migration publishing and add migration file for media table

--- src/MediaServiceProvider.php
+++ src/MediaServiceProvider.php
@@ -1,21 +1,35 @@
 <?php
 
 namespace Bakkali\Media;
-
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Collection;
 class MediaServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         // Publish migration
         $this->publishes([
-            __DIR__.'/../database/migrations/' => database_path('migrations'),
+            __DIR__.'/../database/migrations/create_media_table.php.stub' =>  $this->getMigrationFileName('create_media_table.php'),
         ], 'media-migrations');
     }
 
     public function register()
     {
         //
+    }
+     /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     */
+    protected function getMigrationFileName(string $migrationFileName): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make([$this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR])
+            ->flatMap(fn ($path) => $filesystem->glob($path.'*_'.$migrationFileName))
+            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
+            ->first();
     }
 }
